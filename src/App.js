2e36e63840335490089316825707f56c0af0b8fc
ismiralrasyiid <ismiralrasyiid@gmail.com>
2023-08-05 17:01:54 +0700

    add audio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import FirstLayer from './components/FirstLayer';
 import SecondLayer from './components/SecondLayer';
 import ThirdLayer from './components/ThirdLayer';
 
 function App() {
+  const [ isActive, setIsActive ] = useState(true);
+
+  const onClickButton = () => {
+    const audioElement =  document.getElementById('audio');
+    audioElement.volume = 0.4;
+    audioElement.play();
+
+    setIsActive(!isActive)
+  }
+
   return (
     <>
-      <header>
+      <header className={isActive ? "active" : ""}>
         <h1>Welcome To Our Invitation, Best Regards. Claudia & Richard</h1>
+        <button className={isActive ? "" : "active"} onClick={onClickButton}>Open</button>
       </header>
       <div className="App">
         <FirstLayer />
